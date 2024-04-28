133d5d6795fec13624c73d90695c692b294d807a
sanjaraiy <deepakrai78788@gmail.com>
2024-04-28 23:01:44 +0530

    added featuredCourse Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import FeaturesCourses from "@/components/FeaturesCourses";
 import HeroSection from "@/components/HeroSection";
 
 
@@ -5,6 +6,11 @@ export default function Home() {
   return (
      <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
         <HeroSection></HeroSection>
+        <FeaturesCourses></FeaturesCourses>
+
+
+
+
      </main>
   );
 }
